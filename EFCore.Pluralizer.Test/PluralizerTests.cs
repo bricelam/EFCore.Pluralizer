e664087d2a4111281401ec33b9fb86d2899ea344
Aleksandar Manukov <34056917+aleksandar-manukov@users.noreply.github.com>
2019-06-08 20:16:17 +0300

    Add test cases which show failures with word "Status"

--- EFCore.Pluralizer.Test/PluralizerTests.cs
+++ EFCore.Pluralizer.Test/PluralizerTests.cs
@@ -4,12 +4,22 @@ namespace Bricelam.EntityFrameworkCore.Design
 {
     public class PluralizerTests
     {
-        [Fact]
-        public void Pluralize_works()
-            => Assert.Equal("Tests", new Pluralizer().Pluralize("Test"));
+        [Theory]
+        [InlineData("Tests", "Test")]
+        [InlineData("Statuses", "Status")]
+        [InlineData("ItemStatuses", "ItemStatus")]
+        [InlineData("Statuses", "Statuses")]
+        [InlineData("ItemStatuses", "ItemStatuses")]
+        public void Pluralize_works(string result, string input)
+            => Assert.Equal(result, new Pluralizer().Pluralize(input));
 
-        [Fact]
-        public void Singularize_works()
-            => Assert.Equal("Test", new Pluralizer().Singularize("Tests"));
+        [Theory]
+        [InlineData("Test", "Tests")]
+        [InlineData("Status", "Statuses")]
+        [InlineData("ItemStatus", "ItemStatuses")]
+        [InlineData("Status", "Status")]
+        [InlineData("ItemStatus", "ItemStatus")]
+        public void Singularize_works(string result, string input)
+            => Assert.Equal(result, new Pluralizer().Singularize(input));
     }
 }
